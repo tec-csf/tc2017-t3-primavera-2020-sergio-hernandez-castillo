c383da66d78466bf6d18ac88dbfb79ea22aaa54e
sergio-hernandez-castillo <a01025210@itesm.mx>
2020-04-03 09:21:13 -0600

    Added cpp files that print "Hello World!" to start

---
+++ JobAssignment.cpp
@@ -0,0 +1,26 @@
+/**
+ * Supongamos que disponemos de n trabajadores y n tareas. Sea bij > 0 el coste 
+ * de asignarle el trabajo j al trabajador i. Una asignación de tareas puede ser 
+ * expresada como una asignación de los valores 0 ó 1 a las variables xij, donde 
+ * xij = 0 significa que al trabajador i no le han asignado la tarea j, y xij = 
+ * 1 indica que sí. Una asignación válida es aquella en la que a cada trabajador 
+ * sólo le corresponde una tarea y cada tarea está asignada a un trabajador. Dada 
+ * una asignación válida, definimos el coste de dicha asignación como:
+ * 
+ * ∑n ∑n xijbij
+ * i=1 j=1
+ * 
+ * Diremos que una asignación es óptima si es de mínimo coste
+ * 
+ * Greedy algorithm
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+
+    return 0;
+}//Close main 
---
+++ SubsetSum.cpp
@@ -0,0 +1,17 @@
+/**
+ * Sea W un conjunto de enteros no negativos y M un número entero positivo. 
+ * El problema consiste en diseñar un algoritmo para encontrar todos los 
+ * posibles subconjuntos de W cuya suma sea exactamente M.
+ * 
+ * Backtracking
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World!" << endl;
+
+    return 0;
+}//Close main 
