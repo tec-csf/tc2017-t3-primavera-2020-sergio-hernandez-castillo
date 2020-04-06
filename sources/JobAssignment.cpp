71905b8b5bb99727efedb8b1d4bda41934751d68
sergio-hernandez-castillo <a01025210@itesm.mx>
2020-04-05 21:13:24 -0600

    Fully functional program

--- sources/JobAssignment.cpp
+++ sources/JobAssignment.cpp
@@ -39,15 +39,114 @@
 */
 
 #include <iostream>
+#include <vector>
+#include <time.h>
 
 using namespace std;
 
-int main(){
-    int jobs = 0;
+void fillCosts(vector<vector<int>> &costs, int jobs, int max){
+    srand(time(0));
 
-    cout << "Enter the amount of jobs to be done: ";
+    for (int row = 0; row < jobs; row++){
+        for (int col = 0; col < jobs; col++){
+            costs[row][col] = rand() % max + 1;
+        }//Close for
+    }//Close for
+}//Close fillCosts
+
+bool checkIfFree(vector<vector<int>> &assignations, int jobs, int row){
+    for (int col = 0; col < jobs; col++){
+        if (assignations[row][col] == 1){
+            return false;
+        }//Close if 
+    }//Close for 
+
+    return true;
+}//Close checkIfFree
+
+void printResults(vector<vector<int>> &costs, vector<vector<int>> &assignations, int jobs, int totalCost){
+    cout << "\nThe following is a worker by costs matrix, where a row represents a worker and a column ";
+    cout << "represents the cost of a job: \n" << endl;
+
+    for (int i = 0; i < jobs; i++){
+        for (int j = 0; j < jobs; j++){
+            cout << "[" << costs[i][j] << "]";
+
+            if (j == jobs - 1){
+                cout << endl;
+            }//Close if 
+        }//Close for
+    }//Close for
+
+    cout << "\n";
+
+    for (int row = 0; row < jobs; row++){
+        for (int col = 0; col < jobs; col++){
+            if (assignations[row][col] == 1){
+                cout << "Worker " << row + 1 << " was assigned to job " << col + 1 << " with a cost of " 
+                    << costs[row][col] << endl;
+            }//Close if 
+        }//Close for 
+    }//Close for 
+
+    cout << "\nThe total cost was " << totalCost << endl;
+}//Close printResults
+
+int assignJobs(vector<vector<int>> &costs, vector<vector<int>> &assignations, int jobs, int max){
+    int row;
+    int col;
+    int min = max;
+    int spotX;
+    int spotY;
+    int totalCost = 0;
+    bool isItFree;
+
+    for (col = 0; col < jobs; col++){
+        for (row = 0; row < jobs; row++){
+            isItFree = checkIfFree(assignations, jobs, row);
+
+            if ((costs[row][col] < min) && (isItFree == true)){
+                min = costs[row][col];
+                spotX = row;
+                spotY = col;
+            }//Close if 
+        }//Close for 
+
+        assignations[spotX][spotY] = 1;
+
+        min = max;
+    }//Close for
+
+    for (col = 0; col < jobs; col++){
+        for (row = 0; row < jobs; row++){
+            if (assignations[row][col] == 1){
+                totalCost += costs[row][col];
+            }//Close if
+        }//Close for
+    }//Close for 
+
+    return totalCost;
+}//Close assignJobs
+
+int main(){
+    int jobs;
+    int max;
+    int totalCost;
+
+    cout << "Enter the amount of jobs: ";
     cin >> jobs;
 
-    int costs[jobs][jobs];
-    int assignations[jobs][jobs];
+    cout << "\n";
+
+    cout << "Enter the maximum cost that a job can have: ";
+    cin >> max;
+
+    vector<vector<int>> costs(jobs, vector<int>(jobs));
+    vector<vector<int>> assignations(jobs, vector<int>(jobs));
+
+    fillCosts(costs, jobs, max);
+    totalCost = assignJobs(costs, assignations, jobs, max);
+    printResults(costs, assignations, jobs, totalCost);
+
+    return 0;
 }//Close main 
